80234b1744efb1d5a6e81bc9f366f0e207d0179c
Nicholas Looney <hello@looneymedia.com>
2022-05-30 11:37:18 -0500

    Installed dependencies for StaticImage component. Configured gatsby-config.js with gatsby-plugin-image and -sharp plugins.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home">
       <p>This is a Home Page. I'm learning how Gatsby works.</p>
       <p>It's pretty cool so far.</p>
+      <StaticImage
+        alt="Nick wearing a black, sleeveless shirt."
+        src="../images/profile-pic-nick.png"
+      />
     </Layout>
   )
 }
