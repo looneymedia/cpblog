00ef91a422534b0aeb64e8c0a40e631459281523
Nicholas Looney <hello@looneymedia.com>
2022-05-30 11:11:11 -0500

    Added layout component and css module to wrap individual page contents.

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import { Link } from 'gatsby'
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About">
+        <p>Hi! My name is Nick.</p>
+        <p>I create super dope websites.</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
