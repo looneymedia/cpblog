2218aed6a48119698aa33d2b64bae5c2dc6a35d2
Nicholas Looney <hello@looneymedia.com>
2022-06-01 18:52:51 -0500

    Setup blog folder structure to include image files and renamed posts to index.mdx to avoid duplicating the url slug. Added hero_image data into the frontmatter of each post. Installed and configured gatsby-transformer-sharp childrenImageSharp nodes in GraphiQL and import GatsbyImage component for displaying dynamic images. The getImage() helper function was not working. Looks like it uses childImageSharp whereas the actual data coming through GraphiQL is childrenImageSharp, not child. Extracted gatsbyImageData without getImage(). Images are being displayed dynamically now

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -2,18 +2,36 @@ import React from "react";
 import { graphql } from "gatsby";
 import Layout from "../../components/layout";
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import { GatsbyImage } from "gatsby-plugin-image"; //removed getImage from import - seel below.
 
 const BlogPost = ({ data }) => {
 
-    const { title, author, datePublished } = data.mdx.frontmatter;
     const { body, timeToRead } = data.mdx;
-    console.log(data);
+    const { 
+        title, 
+        author, 
+        datePublished, 
+        hero_image, 
+        hero_image_alt, 
+        hero_image_credit_link, 
+        hero_image_credit_text 
+    } = data.mdx.frontmatter;
+
+    //getImage helper function doesn't seem to work. 
+    //The docs say it returns childImageSharp.gatsbyImageData
+    //However, GraphiQL is returning childrenImageSharp, not child.
+    //So I'm accessing the first and only item in the childrenImageSharp array to get gatsbyImageData.
+
+    // const image = getImage(data.mdx.frontmatter.hero_image);
+    const image = hero_image.childrenImageSharp[0].gatsbyImageData;
 
     return ( 
         <Layout pageTitle={title}>
             <p>{author}</p>
             <p>{datePublished}</p>
             <p>{`${timeToRead} minute read`}</p>
+            <GatsbyImage image={image} alt={hero_image_alt} />
+            <p>Photo by: <a href={hero_image_credit_link}>{hero_image_credit_text}</a></p>
             <MDXRenderer>{body}</MDXRenderer>
         </Layout>
      );
@@ -26,6 +44,14 @@ export const query = graphql`
                 title
                 author
                 datePublished(formatString: "MMMM D, YYYY")
+                hero_image {
+                    childrenImageSharp {
+                        gatsbyImageData
+                    }
+                }
+                hero_image_alt
+                hero_image_credit_link
+                hero_image_credit_text
             }
             body
             timeToRead
